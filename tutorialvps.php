944f9aa532f678a586242532dcbdd6a56ebf9564
avidsaputro <avidsaputro@students.amikom.ac.id>
2021-07-17 13:03:35 +0700

    first commit

---
+++ tutorialvps.php
@@ -0,0 +1,22 @@
+<?php
+$NAMA = "Avid Dwi Saputro";
+$NIM  = "20.11.3322";
+$KELAS= "20-S1IF-01";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Membuat vps azure</title>
+</head>
+<body>
+    <h1>Membuat vps azure</h1>
+    <p><?php echo $NAMA ?></p>
+    <p><?php echo $NIM ?></p>
+    <p><?php echo $KELAS?></p>
+    
+</body>
+</html>
